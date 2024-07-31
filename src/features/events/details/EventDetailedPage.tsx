ceaec7904500cb3f72e41f64aba8d4feaf1c69a5
Micu Adrian <me@i-love-git.com>
2024-07-31 05:42:08 +0300

    End of section 6

--- src/features/events/details/EventDetailedPage.tsx
+++ src/features/events/details/EventDetailedPage.tsx
@@ -1,16 +1,24 @@
-import React from "react";
-import { Grid, GridColumn } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import EventDetailedHeader from "./EventDetailedHeader";
 import EventDetailedChat from "./EventDetailedChat";
 import EventDetailedInfo from "./EventDetailedInfo";
 import EventDetailedSidebar from "./EventDetailedSidebar";
+import { useParams } from "react-router-dom";
+import { useAppSelector } from "../../../app/store/store";
 
 export default function EventDetailedPage() {
+  const { id } = useParams();
+  const event = useAppSelector((state) =>
+    state.events.events.find((e) => e.id === id)
+  );
+
+  if (!event) return <h2>Event not found</h2>;
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventDetailedHeader />
-        <EventDetailedInfo />
+        <EventDetailedHeader event={event} />
+        <EventDetailedInfo event={event} />
         <EventDetailedChat />
       </Grid.Column>
       <Grid.Column width={6}>
