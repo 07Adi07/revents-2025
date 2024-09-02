c7485c6c712bd819bc2ec7fa7f8c52e231ad50da
Micu Adrian <me@i-love-git.com>
2024-09-02 08:52:43 +0300

    End of section 9

--- src/app/common/modals/ModalManager.tsx
+++ src/app/common/modals/ModalManager.tsx
@@ -1,4 +1,5 @@
 import LoginForm from "../../../features/auth/LoginForm";
+import RegisterForm from "../../../features/auth/RegisterForm";
 import TestModal from "../../../features/scratch/TestModal";
 import { useAppSelector } from "../../store/store";
 
@@ -6,16 +7,17 @@ export default function ModalManager() {
   const modalLookup = {
     TestModal,
     LoginForm,
+    RegisterForm,
   };
 
   const { type, data, open } = useAppSelector((state) => state.modals);
 
-  let renderModal;
+  let renderedModal;
 
   if (open && type) {
     const ModalComponent = (modalLookup as any)[type];
-    renderModal = <ModalComponent data={data} />;
+    renderedModal = <ModalComponent data={data} />;
   }
 
-  return <span>{renderModal}</span>;
+  return <span>{renderedModal}</span>;
 }
